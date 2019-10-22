6087a5d80b706b31bdf9ceeb0238fc5f744bc2c9
Teemu Matvejeff <teemu.matvejeff@siili.com>
2019-10-22 15:56:59 +0300

    Tehtävän 5.16 toteutus

---
+++ src/components/Login.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, waitForElement } from '@testing-library/react';
+import App from '../App';
+//jest.mock('./services/blogs');
+
+describe('Integration testing', () => {
+  test('if no user logged, blogs are not rendered', async () => {
+    const component = render(
+      <App />
+    );
+    component.rerender(<App />);
+
+    await waitForElement(
+      () => component.getByText('login')
+    );
+
+    // expectations here
+    expect(component.container).toHaveTextContent('Login into application');
+    expect(component.container).toHaveTextContent('Username');
+    expect(component.container).toHaveTextContent('password');
+    expect(component.container.querySelector('.blogDataShow')).toBe(null);
+  });
+});
